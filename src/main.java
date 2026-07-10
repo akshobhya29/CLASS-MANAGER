18835e06b25fde085d4f63427bcc81cfba6832c5
Akshobhya Raj Singh <akshobhyaraj95@gmail.com>
2026-07-10 22:06:36 +0530

    Added menu using switch case

--- src/main.java
+++ src/main.java
@@ -1,38 +1,58 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-public class gmain
+public class main
 {
     public static void main (String args[])
 { Scanner sc=new Scanner(System.in);
     ArrayList<String> students= new ArrayList<String>();
-    System.out.println("Enter the number of students");
-    int n=sc.nextInt();
-    sc.nextLine();
-    for(int i=1;i<=n;i++)
-    {
-        System.out.println("Enter the student's name");
-        String a=sc.nextLine();
-        students.add(a);
+    System.out.println("       CLASS MANAGER       ");
+    System.out.println("1. Add Students\n2. Display Students\n3. Search Students\n4. Delete Student\n5. EXIT");
+    while(true)
+    {int choice=sc.nextInt();
+    switch(choice)
+    {case 1:
+        System.out.println("Enter the number of students");
+        int n=sc.nextInt();
+        sc.nextLine();
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Enter the student's name");
 
-    }
+        String a=sc.nextLine();
+        students.add(a);}
+        System.out.println("       CLASS MANAGER       ");
+        System.out.println("1. Add Students\n2. Display Students\n3. Search Students\n4. Delete Student\n5. EXIT");
+        break;
+
+        case 2:
     System.out.println(students);
-    System.out.println("If you want to find a Student enter 1 else 0 ");
-    int resp1=sc.nextInt();
-    if(resp1==1)
-    {
-        System.out.println("Enter the roll no. of the student you want to find");
+            System.out.println("       CLASS MANAGER       ");
+            System.out.println("1. Add Students\n2. Display Students\n3. Search Students\n4. Delete Student\n5. EXIT");
+    break;
+        case 3:
+            System.out.println("Enter the roll no. of the student you want to find");
     int rn=sc.nextInt();
     System.out.println("Roll number-"+rn+"       Name-"+students.get(rn-1));
-    }
-    System.out.println("If you want to delete a Student enter 1 else 0");
-    int resp2=sc.nextInt();
-    if(resp2==1){
+            System.out.println("       CLASS MANAGER       ");
+            System.out.println("1. Add Students\n2. Display Students\n3. Search Students\n4. Delete Student\n5. EXIT");
+    break;
+        case 4:
         System.out.println("Enter the roll no. of student you want to delete");
-        int del=sc.nextInt();
-        students.remove(del-1);
-        System.out.println(students);
-
+        int del = sc.nextInt();
+        students.remove(del - 1);
+            System.out.println("       CLASS MANAGER       ");
+            System.out.println("1. Add Students\n2. Display Students\n3. Search Students\n4. Delete Student\n5. EXIT");
+        break;
+        case 5:
+            System.out.println("THE END");
+            return;
+        default:
+            System.out.println("Please enter a valid choice");
+            System.out.println("       CLASS MANAGER       ");
+            System.out.println("1. Add Students\n2. Display Students\n3. Search Students\n4. Delete Student\n5. EXIT");
+    }
+    }
     }
 }
 
-}
+
